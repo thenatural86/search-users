9d8e6d058705fab27793ac91496de0e44a3deca6
thenatural86 <philip.smalls86@gmail.com>
2020-12-29 19:52:10 -0500

    initial render of components on homepage

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import Info from "./Info";
-import Repos from "./Repos";
-import User from "./User";
-import Search from "./Search";
-import Navbar from "./Navbar";
-
-export { Info, Repos, User, Search, Navbar };
+import Info from './Info'
+import Repos from './Repos'
+import User from './User'
+import Search from './Search'
+import Navbar from './Navbar'
+// import components from their files and export with same name
+export { Info, Repos, User, Search, Navbar }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from 'react'
+// import all components from index file in components folder
+// index file is given by default
+import { Info, Repos, User, Search, Navbar } from '../components'
+import loadingImage from '../images/preloader.gif'
+import { GithubContext } from '../context/context'
+
 const Dashboard = () => {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
     </main>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
