62fe7ec59094179dbfb864453a50af2423a31bc8
thenatural86 <philip.smalls86@gmail.com>
2020-12-31 10:06:25 -0500

    render user info

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,12 +3,68 @@ import { GithubContext } from '../context/context'
 import styled from 'styled-components'
 import { GoRepo, GoGist } from 'react-icons/go'
 import { FiUsers, FiUserPlus } from 'react-icons/fi'
+import { items } from 'fusioncharts'
 
 const UserInfo = () => {
   // access pieces of context via useContext hook and passing in the GithubContext
-  const data = React.useContext(GithubContext)
-  console.log(data)
-  return <h2>user info component</h2>
+  const { githubUser } = React.useContext(GithubContext)
+  const { public_repos, followers, following, public_gists } = githubUser
+
+  // array of objects that make up info about user
+  const items = [
+    {
+      id: 1,
+      icon: <GoRepo className='icon' />,
+      label: 'repos',
+      value: public_repos,
+      color: 'pink',
+    },
+    {
+      id: 2,
+      icon: <FiUsers className='icon' />,
+      label: 'followers',
+      value: followers,
+      color: 'green',
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className='icon' />,
+      label: 'following',
+      value: following,
+      color: 'purple',
+    },
+    {
+      id: 4,
+      icon: <GoGist className='icon' />,
+      label: 'gists',
+      value: public_gists,
+      color: 'yellow',
+    },
+  ]
+
+  // iterate over array nd render a Item component
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {items.map((item) => {
+          return <Item key={item.id} {...item} />
+        })}
+      </Wrapper>
+    </section>
+  )
+}
+
+// Item to display with properties from item object de-structured out of {...item}
+const Item = ({ icon, label, value, color }) => {
+  return (
+    <article className='item'>
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  )
 }
 
 const Wrapper = styled.section`
