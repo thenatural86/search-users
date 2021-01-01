45406a79a6f0dd75f7d5694b54e8f4036005caf5
thenatural86 <philip.smalls86@gmail.com>
2021-01-01 16:23:40 -0500

    render language count

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,41 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext)
-  console.log(repos)
+  // console.log(repos)
+
+  // params: total and exact iteration callback function and return an object
+  let languages = repos.reduce(
+    (total, item) => {
+      // console.log(item)
+      // console.log(total)
+      const { language } = item
+      // if language is null return
+      if (!language) return total
+      // if the property does not exist create it and set it's value to 1
+      if (!total[language]) {
+        total[language] = { label: language, value: 1 }
+      } else {
+        // otherwise make a copy of the object and set the value property to its previous value + 1
+        total[language] = {
+          ...total[language],
+          value: total[language].value + 1,
+        }
+      }
+      // return the total, which is an object made up of each item
+      return total
+    },
+    // return an object
+    {}
+  )
+  // console.log(languages)
+  // returns an array of the languages object with its properties values, then sort and slice
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value
+    })
+    .slice(0, 5)
+  // console.log(languages)
+
   const chartData = [
     {
       label: 'poop',
@@ -24,7 +58,7 @@ const Repos = () => {
     <section className='section'>
       <Wrapper className='section-center'>
         {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   )
