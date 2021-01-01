acdb84e2e4b1d935af808fa67e6d81cc3db6366f
thenatural86 <philip.smalls86@gmail.com>
2021-01-01 14:57:18 -0500

    chart data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,8 +5,48 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext)
-  // console.log(repos)
-  return <ExampleChart />
+  console.log(repos)
+  const chartData = [
+    {
+      label: 'poop',
+      value: '290',
+    },
+    {
+      label: 'Saudi',
+      value: '260',
+    },
+    {
+      label: 'Canada',
+      value: '180',
+    },
+    {
+      label: 'Iran',
+      value: '140',
+    },
+    {
+      label: 'Russia',
+      value: '115',
+    },
+    {
+      label: 'UAE',
+      value: '100',
+    },
+    {
+      label: 'US',
+      value: '30',
+    },
+    {
+      label: 'China',
+      value: '30',
+    },
+  ]
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  )
 }
 
 const Wrapper = styled.div`
