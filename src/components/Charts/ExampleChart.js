e03e2adcf129e2682a313c2a2f96b6c28bc8fa41
thenatural86 <philip.smalls86@gmail.com>
2021-01-01 15:25:06 -0500

    render pie chart

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -25,7 +25,7 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme)
 
 const chartComponent = ({ data }) => {
   const chartConfigs = {
-    type: 'column2d', // The chart type
+    type: 'doughnut3d', // The chart type
     width: '700', // Width of the chart
     height: '400', // Height of the chart
     dataFormat: 'json', // Data type
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -19,31 +19,12 @@ const Repos = () => {
       label: 'Canada',
       value: '180',
     },
-    {
-      label: 'Iran',
-      value: '140',
-    },
-    {
-      label: 'Russia',
-      value: '115',
-    },
-    {
-      label: 'UAE',
-      value: '100',
-    },
-    {
-      label: 'US',
-      value: '30',
-    },
-    {
-      label: 'China',
-      value: '30',
-    },
   ]
   return (
     <section className='section'>
       <Wrapper className='section-center'>
-        <ExampleChart data={chartData} />
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={chartData} />
       </Wrapper>
     </section>
   )
