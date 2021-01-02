322bd4cfb6645aa6b25b03dc25239d6e1be82f41
thenatural86 <philip.smalls86@gmail.com>
2021-01-02 10:30:31 -0500

    render 2nd chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -57,8 +57,9 @@ const Repos = () => {
   return (
     <section className='section'>
       <Wrapper className='section-center'>
-        {/* <ExampleChart data={chartData} /> */}
         <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={chartData} />
       </Wrapper>
     </section>
   )
