96f1d08125233ffaad475f59f197cb80b10cf4c3
thenatural86 <philip.smalls86@gmail.com>
2021-01-04 14:48:34 -0500

    private routes

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {/* home route */}
-        <PrivateRoute exact={true} path='/'>
-          <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        {/* wildcard for every other path */}
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Switch>
+          {/* home route */}
+          <PrivateRoute exact={true} path='/'>
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          {/* wildcard for every other path */}
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
   )
 }
 
